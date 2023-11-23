06ba25b82aa936a43561d982a31551aab9895ae1
Priyal Soni <priyal15.soni@gmail.com>
2023-11-22 20:53:19 -0500

    Fixed routing for login and registration functionality - static

--- app.js
+++ app.js
@@ -2,17 +2,10 @@
 const express = require("express") 
 const app=express(); 
 const path = require('path');
-var mongoose=require("mongoose"); 
 var bodyParser=require("body-parser"); 
 
 // Calling form.js from models 
 var Form=require("./models/form"); 
-
-// Connecting to database 
-mongoose.connect("mongodb://localhost/form",{ 
-	useNewUrlParser: true, 
-	useUnifiedTopology: true
-}); 
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'views')));
@@ -26,25 +19,29 @@ app.get("/",function(req,res){
 	res.render("form"); 
 }); 
 
-// form submission 
-app.get('/result',(req,res)=>{ 
-	res.render('result'); 
-}); 
+// Handling registration form submission 
+app.post("/register", function (req, res) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password; // Make sure to handle password securely
 
-//creating form 
-app.post("/",function(req,res){ 
-	var username=req.body.username; 
-	var email=req.body.email; 
-	var f={username: username,email:email}; 
-	Form.create(f,function(err,newlyCreatedForm){ 
-		if(err) 
-		{ 
-			console.log(err); 
-		}else{ 
-			res.redirect("/result"); 
-		} 
-	}); 
-}); 
+    // Create a new user with the form data
+    var newUser = { username: username, email: email, password: password };
+
+    // Handle registration logic (authentication)
+    // For now, just a placeholder response; add MongoDB for future implementation
+    res.send("Register logic not implemented yet.");
+});
+
+// Handling login form submission 
+app.post("/login", function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    // Handle login logic (authentication)
+    // For now, just a placeholder response; add MongoDB for future implementation
+    res.send("Login logic not implemented yet.");
+});
 
 // Starting the server at port 3000 
 app.listen(3000, function() { 
