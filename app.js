7e32393a7ae8eaf69267cf10b14df010ce653fdb
Apratim Shukla <apratimshukla6@gmail.com>
2023-12-12 22:54:43 -0800

    Added handling logic

--- app.js
+++ app.js
@@ -1,50 +1,131 @@
-//importing dependencies 
-const express = require("express") 
-const app=express(); 
+const express = require("express");
+const app = express();
 const path = require('path');
-var bodyParser=require("body-parser"); 
+const bodyParser = require("body-parser");
+const { body, validationResult } = require('express-validator');
+const sqlite3 = require('sqlite3').verbose();
+const ShieldSentry = require('shieldsentry');
+const sentry = new ShieldSentry();
 
-// Calling form.js from models 
-var Form=require("./models/form"); 
+// Initialize the database
+const db = new sqlite3.Database('users.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the users.db database.');
+        initializeDatabase();
+    }
+});
+
+function initializeDatabase() {
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        password TEXT NOT NULL
+    )`, (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            // Check if admin user exists and add if not
+            db.get("SELECT * FROM users WHERE username = 'admin'", function(err, row) {
+                if (!row) {
+                    db.run("INSERT INTO users (username, password) VALUES ('admin', 'adminpass')");
+                }
+            });
+        }
+    });
+}
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'views')));
 
-//middlewares 
-app.set('view engine','ejs'); 
-app.use(bodyParser.urlencoded({extended:true})); 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//rendering form.ejs 
-app.get("/",function(req,res){ 
-	res.render("form"); 
-}); 
-
-// Handling registration form submission 
-app.post("/register", function (req, res) {
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password; // Make sure to handle password securely
-
-    // Create a new user with the form data
-    var newUser = { username: username, email: email, password: password };
-    console.log(newUser); //Ignore the plaintext password for now - would be fixed soon
-
-    // Handle registration logic (authentication)
-    // For now, just a placeholder response; add MongoDB for future implementation
-    res.send("Register logic not implemented yet.");
+app.get("/", function (req, res) {
+    res.render("index");
 });
 
-// Handling login form submission 
-app.post("/login", function (req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-
-    // Handle login logic (authentication)
-    // For now, just a placeholder response; add MongoDB for future implementation
-    res.send("Login logic not implemented yet.");
+app.get("/vulnerable", function (req, res) {
+    res.render("vulnerable");
 });
 
-// Starting the server at port 3000 
-app.listen(3000, function() { 
-	console.log('Server running on port 3000'); 
-}); 
+app.get("/validator", function (req, res) {
+    res.render("validator");
+});
+
+app.post("/", function (req, res) {
+    var { username, password } = req.body;
+
+    // Sanitize inputs using ShieldSentry
+    username = sentry.sqlEscape(username);
+    password = sentry.sqlEscape(password);
+
+    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+
+    db.get(query, (err, user) => {
+        if (err) {
+            let message = "Failed to log in!";
+            res.render("index", { message: message });
+        }
+        if (user) {
+            let message = "Logged in successfully!";
+            res.render("index", { message: message });
+        } else {
+            let message = "Failed to log in!";
+            res.render("index", { message: message });
+        }
+    });
+});
+
+app.post("/vulnerable", function (req, res) {
+    const { username, password } = req.body;
+    const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+
+    db.get(query, (err, user) => {
+        if (err) {
+            let message = "Failed to log in!";
+            res.render("vulnerable", { message: message });
+        }
+        if (user) {
+            let message = "Logged in successfully!";
+            res.render("vulnerable", { message: message });
+        } else {
+            let message = "Failed to log in!";
+            res.render("vulnerable", { message: message });
+        }
+    });
+});
+
+app.post("/validator", 
+    // Validation and sanitization middleware
+    body('username').trim().escape(),
+    body('password').trim().escape(),
+    function (req, res) {
+        // Check for validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render("validator", { message: "Invalid input" });
+        }
+
+        const { username, password } = req.body;
+        const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+
+        db.get(query, (err, user) => {
+            if (err) {
+                let message = "Failed to log in!";
+                res.render("validator", { message: message });
+            }
+            if (user) {
+                let message = "Logged in successfully!";
+                res.render("validator", { message: message });
+            } else {
+                let message = "Failed to log in!";
+                res.render("validator", { message: message });
+            }
+        });
+    }
+);
+
+app.listen(3000, function () {
+    console.log('Server running on port 3000');
+});
